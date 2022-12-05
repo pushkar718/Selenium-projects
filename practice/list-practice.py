d6e3f669a0021874ae94e8293f41100f8ec7445f
wolfie <pushkar2agarwal@gmail.com>
2022-12-05 15:28:15 +0530

    basic form done

--- practice/list-practice.py
+++ practice/list-practice.py
@@ -1,9 +1,9 @@
-import random
-final=str(random.randint(1,12))+str(random.randint(1,31))+str(random.randint(1960,2022))
-if len(final)<=7:
-    final='0'+final
-    if len(final)==7:
-        final=final[:2]+'0'+final[2:]
-final=int(final)
-print(final)
-print(type(final))
+# import random
+# final=str(random.randint(1,12))+str(random.randint(1,31))+str(random.randint(1960,2022))
+# if len(final)<=7:
+#     final='0'+final
+#     if len(final)==7:
+#         final=final[:2]+'0'+final[2:]
+# final=int(final)
+# print(final)
+# print(type(final))
