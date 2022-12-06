661afb693caf20d055514023e5c77ea3fd41deb3
wolfie <pushkar2agarwal@gmail.com>
2022-12-06 17:56:59 +0530

    some edits

--- add-rating/all-rate.py
+++ add-rating/all-rate.py
@@ -1,15 +1,20 @@
 from selenium import webdriver
-# from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 import random
 import time
 
-driver=webdriver.Chrome()
+chrome_options=Options()
+chrome_options.add_argument("--headless")
+driver=webdriver.Chrome(options=chrome_options)
+# driver=webdriver.Chrome()
 action=ActionChains(driver)
 driver.maximize_window()
 driver.get("https://qa.referloan.in")
-time.sleep(3)
+time.sleep(2)
+check_last=driver.find_element(By.XPATH,"(//ul/li/a[@tabindex='-1'])[last()]")
+check_last_url=check_last.get_attribute('href')
 try:
     for b in range(1,500):
         check=driver.find_element(By.XPATH,"(//ul/li/a[@tabindex='-1'])[%d]"%(b))
@@ -17,9 +22,10 @@ try:
         time.sleep(1)
         driver.get(check_url)
         time.sleep(2)
+        page_name = driver.find_element(By.XPATH, "//span[contains(@style,'text-transform')]").text
         driver.execute_script("window.scrollTo(0,250)")
-        time.sleep(3)
-        for i in range(random.randint(80,100)):# Change 65,80 to min,max of rating for 5 star
+        time.sleep(1)
+        for i in range(random.randint(100,120)):# Change 65,80 to min,max of rating for 5 star
             driver.find_element(By.XPATH,"(//span[@class='star'])[5]").click()
         for i in range(random.randint(30,40)):# Change 30,40 to min,max of rating for 4 star
             driver.find_element(By.XPATH,"(//span[@class='star'])[4]").click()
@@ -30,7 +36,9 @@ try:
         for i in range(random.randint(0,3)):  # Change 0,0 to min,max of rating for 1 star
             driver.find_element(By.XPATH, "(//span[@class='star'])[1]").click()
             time.sleep(2)
-            print(check_url, "-----Done-----")
+            print(page_name, "---Done---")
+        if check_url==check_last_url:
+            break
 except(KeyboardInterrupt):
     print("Stopped By User..!")
 except:
