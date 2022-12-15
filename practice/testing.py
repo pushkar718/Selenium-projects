382bdaefc3b433374e6703010fd936ec836d7158
wolfie <pushkar2agarwal@gmail.com>
2022-12-15 16:04:32 +0530

    more dynamic script

--- practice/testing.py
+++ practice/testing.py
@@ -20,7 +20,7 @@ try:
     driver.maximize_window()
     action=ActionChains(driver)
     time.sleep(0.5)
-    temp_url="https://qa.referloan.in/loans/upward-fintech-personal-loan"
+    temp_url="https://qa.referloan.in/loans/fullerton-personal-loan"
     driver.get(temp_url)
     time.sleep(3)
     with py.hold('ctrl'):
@@ -75,13 +75,14 @@ try:
     restart = True
     while restart:
         restart=False
-        for i in range(1,30):
+        for i in range(1,300):
             random_number=random.randint(100000,999999)
             available_option=[]
             element = driver.find_element(By.XPATH,"(//*[contains(@class,'MuiFormControl-root')])[%d]/descendant-or-self::*" % (i))
             element_last = driver.find_element(By.XPATH,"(//*[contains(@class,'MuiFormControl-root')])[last()]/descendant-or-self::*")
             element_inner = element.get_attribute('innerHTML')
             element_text = element.text
+            print(element_text,"DEBUG")
             if 'select' in element_inner:
                 element.click()
                 time.sleep(0.3)
@@ -110,97 +111,100 @@ try:
                 if ('mobile' in element_text.lower())or('phone' in element_text.lower()):
                     element.click()
                     action.key_down(Keys.CONTROL).send_keys('a').key_up(Keys.CONTROL).send_keys(Keys.BACKSPACE).perform()
-                    time.sleep(0.2)
+
                     action.send_keys(int(9667484050)).perform()
                     print(element_text, "->", '9667484050')
                 elif 'address proof' in element_text.lower():
                     element.click()
                     action.key_down(Keys.CONTROL).send_keys('a').key_up(Keys.CONTROL).send_keys(Keys.BACKSPACE).perform()
-                    time.sleep(0.2)
+
                     action.send_keys(int(9667484050)).perform()
                     print(element_text, "->", '9667484050')
                 elif 'email' in element_text.lower():
                     element.click()
                     action.key_down(Keys.CONTROL).send_keys('a').key_up(Keys.CONTROL).send_keys(Keys.BACKSPACE).perform()
-                    time.sleep(0.2)
+
                     action.send_keys("qa@qa.com").perform()
                     print(element_text, "->", 'qa@qa.com')
                 elif ('bank name' in element_text.lower())or('bank code' in element_text.lower()):
                     element.click()
                     action.key_down(Keys.CONTROL).send_keys('a').key_up(Keys.CONTROL).send_keys(Keys.BACKSPACE).perform()
-                    time.sleep(0.2)
+
                     action.send_keys("IDFC").perform()
                     print(element_text, "->", "IDFC")
                 elif 'first name' in element_text.lower():
                     element.click()
                     action.key_down(Keys.CONTROL).send_keys('a').key_up(Keys.CONTROL).send_keys(Keys.BACKSPACE).perform()
-                    time.sleep(0.2)
+
                     action.send_keys(list_name[0]).perform()
                     print(element_text, "->", list_name[0])
                 elif ('last name' in element_text.lower())or('middle name' in element_text.lower()):
                     element.click()
                     action.key_down(Keys.CONTROL).send_keys('a').key_up(Keys.CONTROL).send_keys(Keys.BACKSPACE).perform()
-                    time.sleep(0.2)
+
                     action.send_keys(list_name[1]).perform()
                     print(element_text, "->", list_name[1])
                 elif ('full name' in element_text.lower())or('name' in element_text.lower()):
                     element.click()
                     action.key_down(Keys.CONTROL).send_keys('a').key_up(Keys.CONTROL).send_keys(Keys.BACKSPACE).perform()
-                    time.sleep(0.2)
+
                     action.send_keys(name).perform()
                     print(element_text, "->", name)
                 elif ('amount' in element_text.lower())or('pincode' in element_text.lower())or('salary' in element_text.lower())or('income' in element_text.lower()):
                     element.click()
                     action.key_down(Keys.CONTROL).send_keys('a').key_up(Keys.CONTROL).send_keys(Keys.BACKSPACE).perform()
-                    time.sleep(0.2)
+
                     action.send_keys(random_number).perform()
                     print(element_text, "->", random_number)
                 elif ('months at' in element_text.lower())or('exp' in element_text.lower())or('tenure' in element_text.lower()):
                     element.click()
                     action.key_down(Keys.CONTROL).send_keys('a').key_up(Keys.CONTROL).send_keys(Keys.BACKSPACE).perform()
-                    time.sleep(0.2)
+
                     action.send_keys("8").perform()
                     print(element_text, "->", "8")
                 elif ('address' in element_text.lower())or('landmark' in element_text.lower())or('street' in element_text.lower())or('house' in element_text.lower())or('land mark' in element_text.lower()):
                     element.click()
                     action.key_down(Keys.CONTROL).send_keys('a').key_up(Keys.CONTROL).send_keys(Keys.BACKSPACE).perform()
-                    time.sleep(0.2)
+                    action.send_keys("Dummy Address 123").perform()
+                    print(element_text, "->", "Dummy Address 123")
+                elif 'landmark' in element_text.lower():
+                    element.click()
+                    action.key_down(Keys.CONTROL).send_keys('a').key_up(Keys.CONTROL).send_keys(Keys.BACKSPACE).perform()
                     action.send_keys("Dummy Address 123").perform()
                     print(element_text, "->", "Dummy Address 123")
                 elif ('city' in element_text.lower())or('state' in element_text.lower()):
                     element.click()
                     action.key_down(Keys.CONTROL).send_keys('a').key_up(Keys.CONTROL).send_keys(Keys.BACKSPACE).perform()
-                    time.sleep(0.2)
                     action.send_keys("Delhi").perform()
                     print(element_text, "->", "Delhi")
                 elif ('pan' in element_text.lower())or('pancard' in element_text.lower()):
                     element.click()
                     action.key_down(Keys.CONTROL).send_keys('a').key_up(Keys.CONTROL).send_keys(Keys.BACKSPACE).perform()
-                    time.sleep(0.2)
+
                     action.send_keys(pan_card).perform()
                     print(element_text, "->", pan_card)
                 elif ('document number' in element_text.lower())or('number' in element_text.lower())or('aadhaar' in element_text.lower()):
                     element.click()
                     action.key_down(Keys.CONTROL).send_keys('a').key_up(Keys.CONTROL).send_keys(Keys.BACKSPACE).perform()
-                    time.sleep(0.2)
+
                     action.send_keys("562453123535").perform()
                     print(element_text, "->", "562453123535")
                 elif 'ifsc' in element_text.lower():
                     element.click()
                     action.key_down(Keys.CONTROL).send_keys('a').key_up(Keys.CONTROL).send_keys(Keys.BACKSPACE).perform()
-                    time.sleep(0.2)
+
                     action.send_keys("IDFC00002012").perform()
                     print(element_text, "->", "IDFC00002012")
                 elif 'type' in element_text.lower():
                     element.click()
                     action.key_down(Keys.CONTROL).send_keys('a').key_up(Keys.CONTROL).send_keys(Keys.BACKSPACE).perform()
-                    time.sleep(0.2)
+
                     action.send_keys("Dummy Type").perform()
                     print(element_text, "->", "Dummy Type")
                 elif 'care of' in element_text.lower():
                     element.click()
                     action.key_down(Keys.CONTROL).send_keys('a').key_up(Keys.CONTROL).send_keys(Keys.BACKSPACE).perform()
-                    time.sleep(0.2)
+
                     action.send_keys("Dummy Care").perform()
                     print(element_text, "->", "Dummy Care")
 
@@ -228,7 +232,7 @@ try:
             if element == element_last:
                 driver.find_element(By.XPATH,"//button[@class='mt-4']").click()
                 time.sleep(1)
-                if (driver.find_element(By.XPATH,"//*[@class='loanStep__wrapper']/descendant::*[contains(text(),'Details') or contains(text(),'Thank') or contains(text(),'detail')or contains(text(),'Other')or contains(text(),'other')]")):
+                if (driver.find_element(By.XPATH,"//*[@class='loanStep__wrapper']/descendant::*[contains(text(),'Details') or contains(text(),'Thank') or contains(text(),'detail')or contains(text(),'Other')or contains(text(),'other') or contains(text(),'Info') or contains(text(),'KYC') or contains(text(),'Customer') or contains(text(),'info')]")):
                     if 'Thank You' in driver.find_element(By.TAG_NAME, "html").text:
                         time.sleep(1)
                         screenshot = driver.find_element(By.XPATH, "//div[@class='jumbotron text-center']")
@@ -246,12 +250,12 @@ try:
                         time.sleep(1)
                         break
                     else:
-                        print("-" * 5, driver.find_element(By.XPATH,"//*[@class='loanStep__wrapper']/descendant-or-self::*[contains(text(),'Details') or contains(text(),'Thank') or contains(text(),'detail')or contains(text(),'Other')or contains(text(),'other')]").text, "-" * 5)
+                        print("-" * 5, driver.find_element(By.XPATH,"//*[@class='loanStep__wrapper']/descendant::*[contains(text(),'Details') or contains(text(),'Thank') or contains(text(),'detail')or contains(text(),'Other')or contains(text(),'other') or contains(text(),'Info') or contains(text(),'KYC') or contains(text(),'Customer') or contains(text(),'info')]").text, "-" * 5)
 
                 restart = True
                 break
 
 except Exception as e:
-    time.sleep(2)
+    time.sleep(200)
     print(e)
     driver.close()
