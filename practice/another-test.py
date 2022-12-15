894dbdc2ab2fb1d2538f02f224166ae9bdef4885
wolfie <pushkar2agarwal@gmail.com>
2022-12-15 15:34:53 +0530

    more dynamic script

--- practice/another-test.py
+++ practice/another-test.py
@@ -9,12 +9,15 @@ chrome_options.add_argument("--headless")
 driver=webdriver.Chrome(options=chrome_options)
 # driver=webdriver.Chrome()
 driver.maximize_window()
-while True:
-    driver.get("https://qa.referloan.in")
-    time.sleep(2)
-    if 'Refer' not in driver.title:
-        subprocess.Popen(['notify-send', "QA IS DOWN"])
-        time.sleep(60)
-    else:
-        subprocess.Popen(['notify-send', "QA REFERLOAN IS UP, GET BACK TO WORK"])
-        time.sleep(60)
+try:
+    while True:
+        driver.get("https://qa.referloan.in")
+        time.sleep(2)
+        if 'Refer' not in driver.title:
+            subprocess.Popen(['notify-send', "QA IS DOWN"])
+            time.sleep(60)
+        else:
+            subprocess.Popen(['notify-send', "QA REFERLOAN IS UP, GET BACK TO WORK"])
+            time.sleep(60)
+except KeyboardInterrupt:
+    print("Stopped by user, bye bye")
